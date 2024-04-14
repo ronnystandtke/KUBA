5645eb8358f9172541233fd8310f02580b10d04a
Ronny Standtke <ronny.standtke@gmx.net>
2024-04-15 01:01:14 +0200

    added widgets for selecting how many bridges should be loaded

--- content/KUBA.py
+++ content/KUBA.py
@@ -1,4 +1,3 @@
-# imports
 import folium
 import geopandas as gpd
 import gettext
@@ -30,18 +29,18 @@ CURRENT_YEAR = datetime.now().year
 
 class KUBA:
 
-    statusText = None
     bridges = None
+    osmBridges = None
     earthquakeZones = None
     map = None
 
     def __init__(self):
-        self.statusText = widgets.Text(
-            value='', layout=widgets.Layout(width='600px'), disabled=True)
-        display(self.statusText)
+        statusText = widgets.Text(
+            value='', layout=widgets.Layout(width='95%'), disabled=True)
+        display(statusText)
 
         # read file with data
-        self.statusText.value = _('Converting points to GeoDataFrames, please wait...')
+        statusText.value = _('Loading building data, please wait...')
         df = pd.read_excel(open('data/Bauwerksdaten aus KUBA.xlsx', 'rb'),
                            sheet_name='Alle Brücken mit Zusatzinfos')
 
@@ -49,7 +48,7 @@ class KUBA:
         # print(df.columns.values)
 
         # convert to GeoDataFrame
-        self.statusText.value = _('Converting points to GeoDataFrames, please wait...')
+        statusText.value = _('Converting points to GeoDataFrames, please wait...')
         points = []
         for i in df.index:
             x = df[X_LABEL][i]
@@ -57,9 +56,58 @@ class KUBA:
             points.append(Point(x, y))
         self.bridges = gpd.GeoDataFrame(df, geometry=points, crs='EPSG:2056')
 
-        self.statusText.value = _('Loading earthquake zones, please wait...')
+        statusText.value = _('Loading earthquake zones, please wait...')
         self.earthquakeZones = gpd.read_file(
             "zip://data/erdbebenzonen.zip!Erdbebenzonen")
+
+        # Leaflet always works in EPSG:4326
+        # therefore we have to convert the CRS here
+        statusText.value = _('Converting CRS, please wait...')
+        self.osmBridges = self.bridges.to_crs('EPSG:4326')
+
+        initialWidthStyle = {'description_width': 'initial'}
+        sliderLayout = widgets.Layout(width='95%')
+        self.bridgesSlider = widgets.IntSlider(
+            description=_('Number of bridges'),
+            value=20,
+            max=self.bridges.index.stop,
+            style=initialWidthStyle,
+            layout=sliderLayout
+        )
+
+        self.bridgesIntText = widgets.IntText(
+            description=_('Number of bridges'),
+            style=initialWidthStyle
+        )
+        widgets.link((self.bridgesSlider, 'value'), (self.bridgesIntText, 'value'))
+
+        buttonLayout = widgets.Layout(width='auto')
+        self.loadButton = widgets.Button(
+            description=_('Load selected number of bridges'),
+            layout=buttonLayout
+        )
+
+        # create a progress bar
+        self.progressBar = widgets.IntProgress(
+            value=0,
+            min=0,
+            max=self.bridges.index.stop,
+            description=_('Bridges are being loaded') + ': ' + str(0) + '/' + str(self.bridges.index.stop),
+            description_width=200,
+            # bar_style can be 'success', 'info', 'warning', 'danger' or ''
+            bar_style='success',
+            style={'bar_color': 'green', 'description_width': 'initial'},
+            orientation='horizontal',
+            layout=widgets.Layout(width='auto')
+        )
+
+        self.output = widgets.Output()
+
+        clear_output()
+        display(self.bridgesSlider)
+        display(self.bridgesIntText)
+        display(self.loadButton)
+        display(self.output)
 
     def getNormYear(self, normText):
         if isinstance(normText, str):
@@ -227,59 +275,48 @@ class KUBA:
             else:
                 return 1
 
-    def start(self):
+    def loadBridges(self):
 
+        self.bridgesSlider.disabled = True
+        self.bridgesIntText.disabled = True
+        self.loadButton.disabled = True
+
+        self.progressBar.max = self.bridgesSlider.value
+        self.progressBar.description = _('Bridges are being loaded') + ': ' + str(self.progressBar.value) + '/' + str(self.progressBar.max)
+
+        self.output.clear_output(wait=True)
+        with self.output:
+            display(self.progressBar)
+
+        # create a fresh map (we can't remove existing markers from the map)
         # map = folium.Map(location=[47.15826, 7.27716], tiles="OpenStreetMap", zoom_start=9)
-        self.statusText.value = _('Creating map, please wait...')
         self.map = self.earthquakeZones.explore("ZONE", cmap="OrRd")
 
-        # Leaflet always works in EPSG:4326
-        # therefore we have to convert the CRS here
-        self.statusText.value = _('Converting CRS, please wait...')
-        osmBridges = self.bridges.to_crs('EPSG:4326')
-
-        # create a progress bar
-        progressBar = widgets.IntProgress(
-            value=0,
-            min=0,
-            max=self.bridges.index.stop,
-            description=_('Bridges are being loaded') + ': ' + str(0) + '/' + str(self.bridges.index.stop),
-            description_width=200,
-            # bar_style can be 'success', 'info', 'warning', 'danger' or ''
-            bar_style='success',
-            style={'bar_color': 'green', 'description_width': 'initial'},
-            orientation='horizontal',
-            layout=widgets.Layout(width='auto')
-        )
-
-        display(progressBar)
-
-        # for i in osmBridges.index:
-        for i in range(0, 100):
-            point = osmBridges['geometry'][i]
+        for i in range(0, self.bridgesSlider.value):
+            point = self.osmBridges['geometry'][i]
             # there ARE empty coordinates in the table! :-(
             if not point.is_empty:
 
-                progressBar.value += 1
-                progressBar.description = _('Bridges are being loaded') + ': ' + str(progressBar.value) + '/' + str(progressBar.max)
+                self.progressBar.value += 1
+                self.progressBar.description = _('Bridges are being loaded') + ': ' + str(self.progressBar.value) + '/' + str(self.progressBar.max)
 
-                normYear = self.getNormYear(osmBridges[NORM_YEAR_LABEL][i])
+                normYear = self.getNormYear(self.osmBridges[NORM_YEAR_LABEL][i])
                 normFactor = self.getNormFactor(normYear)
 
-                yearOfConstruction = osmBridges[YEAR_OF_CONSTRUCTION_LABEL][i]
+                yearOfConstruction = self.osmBridges[YEAR_OF_CONSTRUCTION_LABEL][i]
                 age = self.getAge(yearOfConstruction)
                 conditionFactor = self.getConditionFactor(age)
 
-                span = self.getSpan(osmBridges[SPAN_LABEL][i])
+                span = self.getSpan(self.osmBridges[SPAN_LABEL][i])
                 spanFactor = self.getSpanFactor(span)
 
-                typeCode = osmBridges[TYPE_CODE_LABEL][i]
-                typeText = osmBridges[TYPE_TEXT_LABEL][i]
+                typeCode = self.osmBridges[TYPE_CODE_LABEL][i]
+                typeText = self.osmBridges[TYPE_TEXT_LABEL][i]
                 typeFactor = self.getTypeFactor(typeCode)
 
                 materialCode = self.getMaterialCode(
-                    osmBridges[MATERIAL_CODE_LABEL][i])
-                materialText = osmBridges[MATERIAL_TEXT_LABEL][i]
+                    self.osmBridges[MATERIAL_CODE_LABEL][i])
+                materialText = self.osmBridges[MATERIAL_TEXT_LABEL][i]
                 materialFactor = self.getMaterialFactor(materialCode)
 
                 robustnessFactor = self.getRobustnessFactor(yearOfConstruction)
@@ -294,7 +331,7 @@ class KUBA:
                     folium.Marker(
                         location=[point.xy[1][0], point.xy[0][0]],
                         popup=
-                            '<b>' + _('Name') + '</b>: ' + str(osmBridges['Name'][i] + '<br>' +
+                            '<b>' + _('Name') + '</b>: ' + str(self.osmBridges['Name'][i] + '<br>' +
                             '<b>' + _('Year of the norm') + '</b>: ' + (_('unknown') if normYear is None else str(normYear)) + '<br>' +
                             '<b>' + _('Error correction factor') + '</b>: ' + str(normFactor) + '<br>' +
                             '<b>' + _('Age') + '</b>: ' + (_('unknown') if age is None else str(age)) + '<br>' +
@@ -312,5 +349,10 @@ class KUBA:
                     )
                 )
 
-        clear_output()
-        display(self.map)
+        self.output.clear_output(wait=True)
+        with self.output:
+            display(self.map)
+
+        self.bridgesSlider.disabled = False
+        self.bridgesIntText.disabled = False
+        self.loadButton.disabled = False
