091c24a51a683534f77618e1d3231d44a608c60c
Ronny Standtke <ronny.standtke@gmx.net>
2024-07-25 16:57:40 +0200

    updated resize factor

--- content/Plots.py
+++ content/Plots.py
@@ -116,13 +116,13 @@ class Plots:
         # age (x) vs. condition class (y) and probability of collapse (size)
         fig, ax = plt.subplots()
         plt.yticks([1, 2, 3, 4])
-        # the circles became quite large,
-        # therefore we divide the values by this factor
-        resize = 7
+        # the circles became quite small,
+        # therefore we multiply the values by this factor
+        resize = 300
         ax.scatter(
             self.ageConditionPocScatter[self.acpScatterColumns[0]],
             self.ageConditionPocScatter[self.acpScatterColumns[1]],
-            s=self.ageConditionPocScatter[self.acpScatterColumns[2]] / resize)
+            s=self.ageConditionPocScatter[self.acpScatterColumns[2]] * resize)
         ax.set_xlabel(self.acpScatterColumns[0])
         ax.set_ylabel(self.acpScatterColumns[1])
         ax.set_title(self.acpScatterColumns[2])
